45c0af6046243e299b535edda10d74c87a0b75c5
danilomacq <danilomacq@gmail.com>
2021-10-03 12:56:14 -0300

    chore: add base project

--- apps/give-me-cute-animals/src/app/app.module.ts
+++ apps/give-me-cute-animals/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HeaderModule } from './shared/components/header/header.module'
+import { FooterModule } from './shared/components/footer/footer.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HeaderModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
