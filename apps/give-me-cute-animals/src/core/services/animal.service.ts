ba218d517c344201ac9c49f6d17be2786f772990
danilomacq <danilomacq@gmail.com>
2023-10-05 18:09:08 -0300

    fixture: change cat api, add error message

--- apps/give-me-cute-animals/src/core/services/animal.service.ts
+++ apps/give-me-cute-animals/src/core/services/animal.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 import { Animal } from '../models/animal.model';
 
@@ -12,15 +13,58 @@ export class AnimalService {
  constructor(private http: HttpClient) { }
 
   getRandomCat(): Observable<Animal> {
-    return this.http.get('https://aws.random.cat/meow')
+    return this.http.get('https://api.thecatapi.com/v1/images/search').pipe(
+      map((response: any) => {
+        let cat: Animal;
+        if(response && response[0].url){
+          cat = { url: response[0].url };
+        }else{
+          cat = { error: true };
+        }
+        return cat;
+      })
+    );
   }
 
   getRandomDog(): Observable<Animal> {
-    return this.http.get('https://dog.ceo/api/breeds/image/random')
+    return this.http.get('https://dog.ceo/api/breeds/image/random').pipe(
+      map((response: any) => {
+        let dog: Animal;
+        if(response && response.message){
+          dog = { url: response.message };
+        }else{
+          dog = { error: true };
+        }
+        return dog;
+      })
+    );
   }
 
   getRandomFox(): Observable<Animal> {
-    return this.http.get('https://randomfox.ca/floof')
+    return this.http.get('https://randomfox.ca/floof').pipe(
+      map((response: any) => {
+        let fox: Animal;
+        if(response && response.image){
+          fox = { url: response.image };
+        }else{
+          fox = { error: true };
+        }
+        return fox;
+      })
+    );
   }
 
+  getRandomRaccoon(): Observable<Animal> {
+    return this.http.get('https://some-random-api.com/animal/raccoon').pipe(
+      map((response: any) => {
+        let raccoon: Animal;
+        if(response && response.image){
+          raccoon = { url: response.image };
+        }else{
+          raccoon = { error: true };
+        }
+        return raccoon;
+      })
+    );
+  }
 }
