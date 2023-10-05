7be8de49eb2c67ceafe5b1d783036f7f5fc7eebf
danilomacq <danilomacq@gmail.com>
2023-10-05 18:09:59 -0300

    chore: add loading, error msg and click on image to change animal

--- apps/give-me-cute-animals/src/app/app.component.ts
+++ apps/give-me-cute-animals/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { AnimalService } from 'src/core/services/animal.service';
 })
 export class AppComponent {
   animal?: Animal;
+  isLoading = true;
 
   constructor(private animalService: AnimalService) { }
 
@@ -18,27 +19,39 @@ export class AppComponent {
   }
 
   changeAnimal(): void {
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    this.isLoading = true;
+
+    const randomNumber = Math.floor(Math.random() * 4) + 1;
 
     switch (randomNumber) {
       case 1:
         this.animalService.getRandomCat().subscribe((data:Animal) => {
           this.animal = data;
+          this.isLoading = false;
         })
         break;
       case 2:
         this.animalService.getRandomDog().subscribe((data:Animal) => {
           this.animal = data;
+          this.isLoading = false;
         })
         break;
       case 3:
         this.animalService.getRandomFox().subscribe((data:Animal) => {
           this.animal = data;
+          this.isLoading = false;
+        })
+        break;
+      case 4:
+        this.animalService.getRandomRaccoon().subscribe((data:Animal) => {
+          this.animal = data;
+          this.isLoading = false;
         })
         break;
       default:
         this.animalService.getRandomCat().subscribe((data:Animal) => {
           this.animal = data;
+          this.isLoading = false;
         })
         break;
     }
